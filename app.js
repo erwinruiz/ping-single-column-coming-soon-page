9bc434bdd602da3e7e088c51e5055a8a6423f426
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-09-01 10:25:36 -0700

    Add validation of empty input field

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
 const form = document.querySelector("form");
 const emailInput = document.querySelector("form input");
 
-const createError = () => {
+const createError = (text) => {
+  // create the HTML element
   const errorMessage = document.createElement("p");
-
-  errorMessage.textContent = "Please provide a valid email address";
-
+  // insert data
+  errorMessage.textContent = text;
+  // add CSS class
   errorMessage.classList.add("error");
-
+  // insert node
   form.insertBefore(errorMessage, document.querySelector("form button"));
-
+  // add CSS error class to the input field
   emailInput.classList.add("error_border");
 };
 
-const validateEmail = (email) => {
+const validateEmail = () => {
   const reg =
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  // if the error exists and the input field is empty or the email is invalid
+  if (
+    document.querySelector(".error") &&
+    (emailInput.value.trim() === "" || !emailInput.value.match(reg))
+  ) {
+    if (
+      emailInput.value.trim() !== "" &&
+      document.querySelector(".error").textContent !==
+        "Please provide a valid email address"
+    ) {
+      document.querySelector(".error").textContent =
+        "Please provide a valid email address";
+    } else if (
+      emailInput.value.trim() === "" &&
+      document.querySelector(".error").textContent !==
+        "Whoops! It looks like you forgot to add your email"
+    ) {
+      document.querySelector(".error").textContent =
+        "Whoops! It looks like you forgot to add your email";
+    }
+    return;
+  }
+
+  // if the input field is empty and the error doesn't exist
+  if (emailInput.value.trim() === "" && !document.querySelector(".error")) {
+    createError("Whoops! It looks like you forgot to add your email");
+    return;
+  }
+
+  // if the email is invalid and the error doesn't exist
   if (!emailInput.value.match(reg) && !document.querySelector(".error")) {
-    createError();
+    createError("Please provide a valid email address");
   } else if (emailInput.value.match(reg) && document.querySelector(".error")) {
     document.querySelector(".error").remove();
     emailInput.classList.remove("error_border");
